43d042b9393853bb665106c3a5fe3149d9880a28
KennyKen1805 <kenkenny1805@gmail.com>
2025-07-29 19:48:28 +0100

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from "./logo.jpg"
+import search_icon from "./search_icon.png"
+import profile_icon from "./profile_icon.jpg"
+import cart_icon from "./cart_icon.png"
+import menu_icon from "./menu_icon.jpg"
+
+
+
+
+export const assets = {
+    logo,
+    search_icon,
+    profile_icon,
+    cart_icon,
+    menu_icon
+}
