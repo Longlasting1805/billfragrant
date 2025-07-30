c19ece36e8b3cc804efa51f5581fc08f2a11e934
KennyKen1805 <kenkenny1805@gmail.com>
2025-07-30 20:17:16 +0100

    hero bestseller

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,10 +65,10 @@ const Navbar = () => {
                         <img className='h-6 rotate-90' src={assets.dropdown} alt="" />
                         <p>Back</p>
                     </div>
-                    <NavLink className='py-2 p1-6 border' to='/'>HOME</NavLink>
-                    <NavLink className='py-2 p1-6 border' to='/collection'>COLLECTION</NavLink>
-                    <NavLink className='py-2 p1-6 border' to='/about'>ABOUT</NavLink>
-                    <NavLink className='py-2 p1-6 border' to='/contact'>CONTACT</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/contact'>CONTACT</NavLink>
 
                     
 
