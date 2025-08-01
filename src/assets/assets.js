3e7f2da6d1b3c1b45920a5af54f06628b21587d0
KennyKen1805 <kenkenny1805@gmail.com>
2025-08-01 13:39:10 +0100

    product images added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,18 @@ import cart_icon from "./cart_icon.png"
 import menu_icon from "./menu_icon.jpg"
 import dropdown from "./dropdown.png"
 import perfume from "./perfume.jpg"
+import myer from "./myer.jpg"
+import myer2 from "./myer2.jpg"
+import myer3 from "./myer3.jpg"
+import blue from "./blue.jpg"
+import blue1 from "./blue1.jpg"
+import blue2 from "./blue2.jpg"
+import denver1 from "./denver1.jpg"
+import denver2 from "./denver2.jpg"
+import denver3 from "./denver3.jpg"
+import tribal1 from "./tribal1.jpg"
+import tribal2 from "./tribal2.jpg"
+import tribal3 from "./tribal3.jpg"
 
 
 
@@ -17,5 +29,146 @@ export const assets = {
     cart_icon,
     menu_icon, 
     dropdown, 
-    perfume
+    perfume,
+    myer,
+    myer2,
+    myer3,
+    blue,
+    blue1,
+    blue2, 
+    denver1,
+    denver2,
+    denver3,
+    tribal1,
+    tribal2,
+    tribal3
 }
+
+export const products = [
+    {
+        _id: "a1",
+        name: "Myer",
+        description: "A lightweight peff",
+        price: 100,
+        image: [myer, myer2, myer3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a2",
+        name: "Blue",
+        description: "A lightweight peff",
+        price: 100,
+        image: [blue, blue1, blue2],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+    {
+        _id: "a3",
+        name: "Denver",
+        description: "A lightweight peff",
+        price: 100,
+        image: [denver1, denver2, denver3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+    {
+        _id: "a4",
+        name: "Tribal",
+        description: "A lightweight peff",
+        price: 100,
+        image: [tribal1, tribal2, tribal3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+    {
+        _id: "a5",
+        name: "Myer",
+        description: "A lightweight peff",
+        price: 100,
+        image: [myer, myer2, myer3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a6",
+        name: "Blue",
+        description: "A lightweight peff",
+        price: 100,
+        image: [blue, blue1, blue2],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a7",
+        name: "Denver",
+        description: "A lightweight peff",
+        price: 100,
+        image: [denver1, denver2, denver3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a8",
+        name: "Tribal",
+        description: "A lightweight peff",
+        price: 100,
+        image: [tribal1, tribal2, tribal3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a9",
+        name: "Myer",
+        description: "A lightweight peff",
+        price: 100,
+        image: [myer, myer2, myer3],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    },
+
+    {
+        _id: "a10",
+        name: "Blue",
+        description: "A lightweight peff",
+        price: 100,
+        image: [blue, blue1, blue2],
+        category: "Men",
+        subCategory: "Men Pefume",
+        Sizes: ["S", "M", "L"],
+        date: 17166345448,
+        bestseller: true
+    }
+]
