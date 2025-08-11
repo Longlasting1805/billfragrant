4c7658a151cc42b08f7b74d007b70fb09ed19651
KennyKen1805 <longlasting1805@gmail.com>
2025-08-11 10:11:51 +0100

    searchbar should only be visible on collection page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false)
-    const {setShowSearch} = useContext(ShopContext)
+    const { setShowSearch } = useContext(ShopContext)
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
-           <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link> 
+            <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -34,7 +34,7 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-7 cursor-pointer' alt="" />
+                <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-7 cursor-pointer' alt="" />
 
                 <div className='group relative'>
                     <img src={assets.profile_icon} className='w-8 cursor-pointer' alt="" />
@@ -43,12 +43,9 @@ const Navbar = () => {
                             <p className='cursor-pointer  hover:text-black '>My Profile</p>
                             <p className='cursor-pointer  hover:text-black '>Orders</p>
                             <p className='cursor-pointer  hover:text-black '>Logout</p>
-
                         </div>
                     </div>
-
                 </div>
-
                 <Link to="/cart" className='relative'>
                     <img src={assets.cart_icon} className='w-6 min-w--5' alt="" />
                     <p className='absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
@@ -62,10 +59,10 @@ const Navbar = () => {
                         <img className='h-6 rotate-90' src={assets.dropdown} alt="" />
                         <p>Back</p>
                     </div>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/'>HOME</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/collection'>COLLECTION</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/about'>ABOUT</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/contact'>CONTACT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 p1-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 p1-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 p1-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 p1-6 border' to='/contact'>CONTACT</NavLink>
 
                 </div>
 
